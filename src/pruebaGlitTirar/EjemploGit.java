a59d4ba87174f2c77bdd6dbf24abe893dd9ba82b
Belisario Buitrago Lievano <bbltrabajo@gmail.com>
2023-11-27 20:12:51 -0500

    Comienzo de Proyecto

---
+++ src/pruebaGlitTirar/EjemploGit.java
@@ -0,0 +1,11 @@
+
+package pruebaGlitTirar;
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola alumnos del curso Git");
+	}
+
+}
