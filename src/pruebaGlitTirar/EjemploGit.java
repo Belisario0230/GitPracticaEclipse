84254ee67099e89b5a531e994fd49209ff74108f
Belisario Buitrago Lievano <bbltrabajo@gmail.com>
2023-11-27 20:16:13 -0500

    Agregar Variables

--- src/pruebaGlitTirar/EjemploGit.java
+++ src/pruebaGlitTirar/EjemploGit.java
@@ -6,6 +6,8 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola alumnos del curso Git");
+		
+		String nombre = "Belisario Buitrago Lievano";
 	}
 
 }
